ffdeecdd8ceafe5e3be3dd3685e57f0b90e707c0
Davide <58419266+davideangelone@users.noreply.github.com>
2021-04-27 10:48:49 +0200

    Add files via upload

---
+++ src/services/auth.service.js
@@ -0,0 +1,42 @@
+import axios from "axios";
+
+const API_URL = "http://localhost:8082/api/auth/";
+
+class AuthService {
+  login(username, password) {
+    return axios
+      .post(API_URL + "signin", {
+        username,
+        password,
+      })
+      .then((response) => {
+        if (response.data.accessToken) {
+          localStorage.setItem("user", JSON.stringify(response.data));
+        }
+
+        return response.data;
+      });
+  }
+
+  logout() {
+    localStorage.removeItem("user");
+  }
+
+  register(username, email, password, firstName, lastName, age, nationality) {
+    return axios.post(API_URL + "signup", {
+      username,
+      email,
+      password,
+      firstName,
+      lastName,
+      age,
+      nationality,
+    });
+  }
+
+  getCurrentUser() {
+    return JSON.parse(localStorage.getItem("user"));
+  }
+}
+
+export default new AuthService();
